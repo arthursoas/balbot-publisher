76e0a414e57ce4b210feaf06d1c1c0c9475cfdd2
Arthur Soares <arthurvsp97@gmail.com>
2020-11-21 22:35:19 -0300

    minor ajust on index

--- src/index.ts
+++ src/index.ts
@@ -37,8 +37,7 @@ class Program
 
         const publishings: Array<Promise<void>> = chatbots.map(async (chatbot) => {
             try {
-                await this._blipService
-                    .UpdateChatbotFlowAsync(chatbot, balbotFlow)
+                await this._blipService.UpdateChatbotFlowAsync(chatbot, balbotFlow)
             }
             catch (exception: any) {
                 this._loggerService.Error(

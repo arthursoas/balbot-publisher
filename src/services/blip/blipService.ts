6d51cb25cd7f843ea2834457bd19b731b696466a
Arthur Soares <arthurvsp97@gmail.com>
2020-11-28 17:55:43 -0300

    Added comments explaining what each blip command do

--- src/services/blip/blipService.ts
+++ src/services/blip/blipService.ts
@@ -17,6 +17,7 @@ export class BlipService implements IBlipService {
     public async PublishChatbotFlowAsync(chatbot: Chatbot, flow: any): Promise<void> {
         const application: any = BlipOperations.ConvertFlowToApplication(chatbot, flow);
         const configurationRequests: Array<Promise<void>> = [
+            // Publish flow effectively
             this.MakeBlipHttpRequestAsync(
                 chatbot,
                 'set',
@@ -27,29 +28,34 @@ export class BlipService implements IBlipService {
                     Application: {...application}
                 }
             ),
+            // Save published flow on bucket for restoration purposes
             this.MakeBlipHttpRequestAsync(
                 chatbot,
                 'set',
                 '/buckets/blip_portal:builder_published_flow',
                 'application/json',
                 flow
-            )
-        ];
-        const configurationUris: string[] = [
-            '/buckets/blip_portal:builder_working_configuration',
-            '/buckets/blip_portal:builder_published_configuration'
-        ]
-        configurationUris.forEach(async (configurationUri) => {
-            configurationRequests.push(this.MakeBlipHttpRequestAsync(
+            ),
+            // Save current configuration on bucket for restoration purposes
+            this.MakeBlipHttpRequestAsync(
                 chatbot,
                 'set',
-                configurationUri,
+                '/buckets/blip_portal:builder_working_configuration',
                 'application/json',
                 application.settings.flow.configuration
-            ));
-        });
+            ),
+            // Save published configuration on bucket for restoration purposes
+            this.MakeBlipHttpRequestAsync(
+                chatbot,
+                'set',
+                '/buckets/blip_portal:builder_published_configuration',
+                'application/json',
+                application.settings.flow.configuration
+            )
+        ];
         await Promise.all(configurationRequests);
 
+        // Get flow versions lists
         const publications = await this.MakeBlipHttpRequestAsync(
             chatbot,
             'get',
@@ -60,6 +66,7 @@ export class BlipService implements IBlipService {
         );
         const newPublications = BlipOperations.AddReleaseToPublications(publications)
         const latestPublicationsRequests: Array<Promise<void>> = [
+            // Update the latest version flow for restoration purpose
             this.MakeBlipHttpRequestAsync(
                 chatbot,
                 'set',
@@ -67,6 +74,7 @@ export class BlipService implements IBlipService {
                 'application/json',
                 newPublications
             ),
+            // Add publishing to versioning list
             this.MakeBlipHttpRequestAsync(
                 chatbot,
                 'set',
